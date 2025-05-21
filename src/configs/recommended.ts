5727c59bbfb831806bcd95ae9bda0ce3f6dd7789
Amnish Singh Arora <amnishsingh04@gmail.com>
2025-05-20 21:00:54 -0400

    Fix `recommended` config export to support flat config

--- src/configs/recommended.ts
+++
@@ -1,6 +0,0 @@
-export const recommended = {
-    plugins: ["error-cause"],
-    rules: {
-        "error-cause/no-swallowed-error-cause": "error",
-    },
-};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-import configs from "./configs";
 import packageJson from "../package.json";
 import rules from "./rules";
+
 const meta = { name: packageJson.name, version: packageJson.version };
 
 const plugin = {
     meta,
-    configs,
+    configs: {},
     rules,
     processors: {},
 };
 
-export default plugin;
+Object.assign(plugin.configs, {
+    recommended: [
+        {
+            plugins: {
+                "error-cause": plugin,
+            },
+            rules: {
+                "error-cause/no-swallowed-error-cause": "warn",
+            },
+        },
+    ],
+});
+
+module.exports = plugin;
